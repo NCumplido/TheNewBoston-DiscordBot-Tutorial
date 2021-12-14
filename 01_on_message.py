5c2a344afc5a01b2bb93aacf7fcd474918238e07
Nicholas Cumplido <ncumplido@msn.com>
2021-12-14 10:49:44 +0000

    Remove Key
    Remove Key

--- 01_on_message.py
+++ 01_on_message.py
@@ -16,4 +16,4 @@ async def on_message(message):
     if message.content == 'Hello' or 'hello' or 'hi' or 'Hi' or 'hi':
         await message.channel.send('Welcome!')
 
-client.run('ODkxNjE0NDE5OTU4Nzg0MDEz.YVA6ow.LlH0EVj-0jsPZSTdg0_Sz2_7qGM')
+client.run('Remove Key')
